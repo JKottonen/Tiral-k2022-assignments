b0b54b355d48fb689ed0d691eb78ba330fa24b4a
Jesse Kottonen <jesse.kottonen@gmail.com>
2022-01-12 15:31:24 +0200

    Renamed alkuluvut to primes and completed the assignment.

--- viikko-1/Alkuluvut.py
+++
@@ -1,9 +0,0 @@
-def count(n):
-    pass
-
-if __name__ == "__main__":
-    print(count(2)) # 1
-    print(count(10)) # 4
-    print(count(11)) # 5
-    print(count(100)) # 25
-    print(count(1000)) # 168
