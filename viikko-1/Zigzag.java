0413e83da0ec13c03f5cf2247cd1cc27a8133e11
Jesse Kottonen <jesse.kottonen@gmail.com>
2022-01-12 19:21:32 +0200

    zigzag makes a string of numbers right

--- viikko-1/Zigzag.java
+++ viikko-1/Zigzag.java
@@ -2,32 +2,28 @@ import java.util.*;
 
 public class Zigzag {
     public int[] create(int n) {
-        int[] zigzaglist = new int[n];
-        int center = Math.floorDiv(n, 2);
-        int offset = 0;
-        boolean rightSide = false;
-
-        for (int i = 0; i < n; i++) {
-            if(rightSide) {
-                zigzaglist[center + offset] = i + 1;
-                rightSide = !rightSide;
+        String str = "";
+        int[] list = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                str = Integer.toString(i + 1) + str;
             }
             else {
-                zigzaglist[center - offset] = i + 1;
-                rightSide = !rightSide;
-                offset ++;
+                str = str + Integer.toString(i + 1);
             }
         }
-
-        return zigzaglist;
+        
+        System.out.println(str);
+        return list;
     }
 
     public static void main(String[] args) {
         Zigzag z = new Zigzag();
         System.out.println(Arrays.toString(z.create(1))); // [1]
-//        System.out.println(Arrays.toString(z.create(2))); // [1,2]
+        System.out.println(Arrays.toString(z.create(2))); // [1,2]
         System.out.println(Arrays.toString(z.create(3))); // [3,1,2]
-  //      System.out.println(Arrays.toString(z.create(4))); // [3,1,2,4]
+        System.out.println(Arrays.toString(z.create(4))); // [3,1,2,4]
         System.out.println(Arrays.toString(z.create(5))); // [5,3,1,2,4]
     }
 }
