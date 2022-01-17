955492cd6b5b1a88517df471bd052fc6c12f0d86
Jesse Kottonen <jesse.kottonen@gmail.com>
2022-01-17 20:03:28 +0200

    knight is now able to avoid queen on up-down and left-right -axis

--- viikko-1/queenknight.py
+++ viikko-1/queenknight.py
@@ -1,20 +1,41 @@
-from logging.handlers import QueueListener
-
-
 def printMatrix(matrix: list):
     for line in matrix:
         print(line)
+    print()
+
+def possible_knight_positions(n, matrix, queenY, queenX):
+    sum = 0
+    for y in range(n):
+        if y == queenY:
+            continue
+        for x in range(n):
+            if x == queenX:
+                continue
+            matrix[y][x] = 2
+            sum += 1
+            printMatrix(matrix)
+            matrix[y][x] = 0
+            
     
+    return sum
+
+
 
 def count(n: int):
+    sum = 0
     matrix = [[0 for i in range(n)] for j in range(n)]
     queen = 1
-    knight = 2
-    matrix[1][0] = queen
-    matrix[3][3] = knight
-    printMatrix(matrix)
+
+    for y in range(n):
+        for x in range(n):
+            matrix[y][x] = queen
+            sum += possible_knight_positions(n, matrix, y, x)
+            matrix[y][x] = 0
+
+    return sum
+
 
 if __name__ == "__main__":
 #    print(count(3)) # 0
-    print(count(4)) # 40
+    print(count(3)) # 40
 #    print(count(5)) # 184
