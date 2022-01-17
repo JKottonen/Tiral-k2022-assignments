65a7363f5d005e3715906db2fe03523ef4ecfb3c
Jesse Kottonen <jesse.kottonen@gmail.com>
2022-01-17 20:43:19 +0200

    queenknight works completely.

--- viikko-1/queenknight.py
+++ viikko-1/queenknight.py
@@ -1,4 +1,10 @@
+from os import system
+import os
+
+
 def printMatrix(matrix: list):
+    input("")
+    os.system("cls")
     for line in matrix:
         print(line)
     print()
@@ -6,17 +12,29 @@ def printMatrix(matrix: list):
 def possible_knight_positions(n, matrix, queenY, queenX):
     sum = 0
     for y in range(n):
+        # Y axis not valid:
         if y == queenY:
             continue
+
         for x in range(n):
+            # X axis not valid:
             if x == queenX:
                 continue
+            
+            # Diagonal axis not valid:
+            if x - y == queenX - queenY or x + y == queenX + queenY:
+                continue
+            
+            # This one cover's the knights possible movements
+            # (and two squares on X or Y axis, which doesn't matter because the queen makes them invalid anyways)
+            if abs(queenY - y) <= 2 and abs(queenX - x) <= 2:
+                continue
+
             matrix[y][x] = 2
             sum += 1
-            printMatrix(matrix)
+            #printMatrix(matrix)
             matrix[y][x] = 0
             
-    
     return sum
 
 
@@ -36,6 +54,6 @@ def count(n: int):
 
 
 if __name__ == "__main__":
-#    print(count(3)) # 0
-    print(count(3)) # 40
-#    print(count(5)) # 184
+    print(count(3)) # 0
+    print(count(4)) # 40
+    print(count(5)) # 184
