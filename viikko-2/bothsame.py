dfa494d5661d3a4c1f264ef8cfac2e6c71485dd2
Jesse Kottonen <jesse.kottonen@gmail.com>
2022-01-25 09:23:19 +0200

    Broke and fixed bothsame.py again.

--- viikko-2/bothsame.py
+++ viikko-2/bothsame.py
@@ -10,9 +10,9 @@ def count(s):
         else:
             distinct[i] += 1
             sum += distinct[i]
-
+ 
     return sum
-
+ 
 if __name__ == "__main__":
     print(count("aaa")) # 6
     print(count("abcd")) # 4
